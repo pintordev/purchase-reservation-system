75ca51707f94a6853305b8ff150c02e3f5922f13
pintor.dev <pintor.dev@gmail.com>
2024-04-18 11:43:07 +0900

    env setting

---
+++ src/main/java/com/pintor/purchase_reservation_system/PurchaseReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pintor.purchase_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseReservationSystemApplication.class, args);
+	}
+
+}
