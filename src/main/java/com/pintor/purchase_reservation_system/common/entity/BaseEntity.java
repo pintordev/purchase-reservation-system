f88decb35bf3090fa90429777d7f39be397b2178
pintor.dev <pintor.dev@gmail.com>
2024-04-18 18:00:17 +0900

    feature/member-signup #1 - entity setting

---
+++ src/main/java/com/pintor/purchase_reservation_system/common/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.pintor.purchase_reservation_system.common.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@EqualsAndHashCode(of = "id")
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
