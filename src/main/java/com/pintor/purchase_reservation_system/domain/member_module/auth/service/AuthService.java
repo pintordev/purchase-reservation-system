f04619ad6cdc4db4231d7e7df31963259b41d267
pintor.dev <pintor.dev@gmail.com>
2024-04-20 20:57:39 +0900

    feature/auth-login #2 login validate

--- src/main/java/com/pintor/purchase_reservation_system/domain/member_module/auth/service/AuthService.java
+++ src/main/java/com/pintor/purchase_reservation_system/domain/member_module/auth/service/AuthService.java
@@ -3,10 +3,13 @@ package com.pintor.purchase_reservation_system.domain.member_module.auth.service
 import com.pintor.purchase_reservation_system.common.errors.exception.ApiResException;
 import com.pintor.purchase_reservation_system.common.response.FailCode;
 import com.pintor.purchase_reservation_system.common.response.ResData;
+import com.pintor.purchase_reservation_system.common.service.EncryptService;
 import com.pintor.purchase_reservation_system.domain.member_module.auth.entity.MailToken;
 import com.pintor.purchase_reservation_system.domain.member_module.auth.repository.MailTokenRepository;
 import com.pintor.purchase_reservation_system.domain.member_module.auth.request.AuthLoginRequest;
 import com.pintor.purchase_reservation_system.domain.member_module.auth.response.AuthLoginResponse;
+import com.pintor.purchase_reservation_system.domain.member_module.member.entity.Member;
+import com.pintor.purchase_reservation_system.domain.member_module.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 
 import java.security.SecureRandom;
+import java.util.Optional;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -22,6 +26,9 @@ import java.security.SecureRandom;
 public class AuthService {
 
     private final MailTokenRepository mailTokenRepository;
+    private final MemberRepository memberRepository;
+
+    private final EncryptService encryptService;
 
     @Transactional
     public String saveMailToken(Long memberId) {
@@ -62,6 +69,8 @@ public class AuthService {
 
     @Transactional
     public AuthLoginResponse login(AuthLoginRequest request, BindingResult bindingResult) {
+        
+        Member member = this.loginValidate(request, bindingResult);
 
         // TODO : Implement login logic
         String accessToken = this.authService.getAccessToken(member.getId());
@@ -69,5 +78,53 @@ public class AuthService {
         this.authService.saveAuthToken(member.getId(), refreshToken, accessToken);
 
         return AuthLoginResponse.of(accessToken, refreshToken);
+    }
+
+    private Member loginValidate(AuthLoginRequest request, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+
+            log.error("binding error: {}", bindingResult);
+
+            throw new ApiResException(
+                    ResData.of(
+                            FailCode.BINDING_ERROR,
+                            bindingResult
+                    )
+            );
+        }
+
+        Member member = this.memberRepository.findByEmail(request.getEmail())
+                .orElse(null);
+
+        if (member == null) {
+
+            bindingResult.rejectValue("email", "member not found from email");
+
+            log.error("member not found from email: {}", bindingResult);
+
+            throw new ApiResException(
+                    ResData.of(
+                            FailCode.MEMBER_NOT_FOUND,
+                            bindingResult
+                    )
+            );
+        }
+
+        if (!this.encryptService.passwordMatches(request.getPassword(), member.getPassword())) {
+
+            bindingResult.rejectValue("password", "password not match");
+
+            log.error("password not match: {}", bindingResult);
+
+            throw new ApiResException(
+                    ResData.of(
+                            FailCode.PASSWORD_NOT_MATCH,
+                            bindingResult
+                    )
+            );
+        }
+
+        return member;
     }
 }
--- src/main/java/com/pintor/purchase_reservation_system/domain/member_module/member/repository/MemberRepository.java
+++ src/main/java/com/pintor/purchase_reservation_system/domain/member_module/member/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package com.pintor.purchase_reservation_system.domain.member_module.member.repos
 import com.pintor.purchase_reservation_system.domain.member_module.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
