94eaa210e2a4198e841c3bef84fdae3fceea03f7
pintor.dev <pintor.dev@gmail.com>
2024-04-22 00:56:07 +0900

    Revert "feature/auth-logout"

--- src/main/java/com/pintor/purchase_reservation_system/domain/member_module/auth/repository/MailTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pintor.purchase_reservation_system.domain.member_module.auth.repository;
-
-import com.pintor.purchase_reservation_system.domain.member_module.auth.entity.MailToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MailTokenRepository extends CrudRepository<MailToken, String> {
-}
