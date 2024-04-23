a5a1df845b36d746d241cdea16ae480c865b8ce5
pintor.dev <pintor.dev@gmail.com>
2024-04-23 14:38:26 +0900

    feature/cart_item-delete #2 - service setup

--- src/main/java/com/pintor/purchase_reservation_system/domain/purchase_module/cart_item/service/CartItemService.java
+++ src/main/java/com/pintor/purchase_reservation_system/domain/purchase_module/cart_item/service/CartItemService.java
@@ -101,4 +101,12 @@ public class CartItemService {
                         )
                 ));
     }
+
+    @Transactional
+    public void delete(Long id) {
+
+        CartItem cartItem = this.getCartItem(id);
+
+        this.cartItemRepository.delete(cartItem);
+    }
 }
