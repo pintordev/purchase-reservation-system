9582f93b06cca0c9ccdc78c85dab2459b1a9b4da
pintor.dev <pintor.dev@gmail.com>
2024-05-07 19:43:32 +0900

    build: payment-module build setting

---
+++ payment-module/src/test/java/com/pintor/payment_module/PaymentModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintor.payment_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
